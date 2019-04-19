6a1c814cfe934a03f2ce3df01a8628bed7671281
Jason <xc_202@sina.com>
2019-04-19 17:44:59 +0800

    add unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
+import unittest
+class NewVistorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test1(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
+print(__name__)
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'Django' in browser.title
