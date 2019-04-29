f54f1a1b345afe5b3f0420f46b1f0b908d13eaa6
Jason <xc_202@sina.com>
2019-04-29 16:42:56 +0800

    function test Refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -7,33 +7,40 @@ class NewVistorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
-        '''
-    def test1(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title)
-        #self.fail('Finish the test!')
-        '''
+    def check_for_row_in_list_table(self,row_test):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_test,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        data = ["1:Buy peacock feathers","2:Use peacock feathers to make a fly"]
+        for i in range(0,len(data)):
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+            inputbox.send_keys(data[i])
+            inputbox.send_keys(Keys.ENTER)
+            time.sleep(3)
+            self.check_for_row_in_list_table(data[i])
+    #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #text=tr.find_elements_by_tag_name("td")[0].text
+        #print(text)
         '''
         self.assertTrue(
             any(row.text == '1:Buy peacock feathers' for row in rows),
             f"New to-do item did not appear in table.Content were:\n{table.text}"
             )
+        
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+        time.sleep(3)
         '''
-        self.assertIn('Buy peacock feathers',[row.text for row in rows])
-        print(row.text)
-        time.sleep(10)
         #self.fail('Finish the test!')
 #print(__name__)
 if __name__ =='__main__':
--- lists/views.py
+++ lists/views.py
@@ -6,5 +6,5 @@ def home_page(self):
     #return HttpResponse('<html><title>To-Do lists</title></html>')
     #if self.method == 'POST':
      #   return HttpResponse(self.POST['item_text'])
-    return render(self,'home.html',{'new_item_text': self.POST.get('item_text','')})
+    return render(self,'home.html',{'aabb':self.POST.get('item_text','')})
 #home_page = None
